c2b822469d4f7d547144143c8c02bb4dddcfba4f
Enrique Delgado <delgado.enrique@gmail.com>
2013-06-27 19:07:36 -0500

    Fixtures of people.

---
+++ server/fixtures.js
@@ -0,0 +1,46 @@
+if (People.find().count() === 0) {
+  People.insert({
+    name: 'Enrique',
+    email: 'delgado.enrique@gmail.com'
+  });
+
+  People.insert({
+    name: 'Elizabeth',
+    email: 'elizabeth@example.com'
+  });
+  
+  People.insert({
+    name: 'Sophia',
+    email: 'sophia@example.com'
+  });
+  
+  People.insert({
+    name: 'Ludwig',
+    email: 'ludwig@example.com'
+  });
+  
+  People.insert({
+    name: 'Vinny',
+    email: 'vinny@example.com'
+  });
+  
+  People.insert({
+    name: 'Stacey',
+    email: 'stacey@example.com'
+  });
+
+  People.insert({
+    name: 'Angela',
+    email: 'angela@example.com'
+  });
+
+  People.insert({
+    name: 'Casey',
+    email: 'casey@example.com'
+  });
+
+  People.insert({
+    name: 'William',
+    email: 'william@example.com'
+  });
+}
