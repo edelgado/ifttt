af0dbd2c8c337f336ccdfd8d2ee9643c2df35e7e
Enrique Delgado <delgado.enrique@gmail.com>
2013-06-29 17:55:54 -0500

    You can now add and remove people from teams from the UI.

--- client/views/admin/membership_status.js
+++ client/views/admin/membership_status.js
@@ -16,3 +16,34 @@ Template.membershipStatus.helpers({
     }
   }
 });
+Template.membershipStatus.events({
+  'change input[type=checkbox]': function(e) {    
+    var op, pId, tId;
+    
+    if ($(e.target).is(':checked')) {
+      op = 'add';
+    } else {
+      op = 'delete';
+    }
+    
+    if (adminCurrentCollectionName() == 'People') {
+      pId = Session.get('currentItemId');
+      tId = this._id;
+    } else if (adminCurrentCollectionName() == 'Teams') {
+      pId = this._id;
+      tId = Session.get('currentItemId');
+    }
+    
+    switch(op) {
+      case 'add':
+        Memberships.insert({personId: pId, teamId: tId});
+        break;
+      case 'delete':
+        var existingRecords = Memberships.find({personId: pId, teamId: tId}).fetch();
+        _.each(existingRecords, function(element, index, list){
+          Memberships.remove(element._id);
+        });
+        break;
+    }
+  }
+});
