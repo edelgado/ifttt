d6fe231663ff79ec9cb59fa5d5a1c6a8ce4a4e71
Enrique Delgado <delgado.enrique@gmail.com>
2013-06-29 17:55:10 -0500

    Added team memberships to the fixtures.

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,10 +1,5 @@
 if (People.find().count() === 0) {
   People.insert({
-    name: 'Enrique',
-    email: 'delgado.enrique@gmail.com'
-  });
-
-  People.insert({
     name: 'Elizabeth',
     email: 'elizabeth@example.com'
   });
@@ -53,4 +48,73 @@ if (Teams.find().count() === 0) {
   Teams.insert({
     name: 'Team 2'
   });
+}
+
+if (Memberships.find().count() === 0) {
+  // Team 1 members
+  var team = Teams.findOne({name: 'Team 1'});
+  // Add Elizabeth
+  var person = People.findOne({email: 'elizabeth@example.com'});
+  Memberships.insert({
+    personId: person._id,
+    teamId: team._id
+  });
+  
+  // Add Sophia
+  var person = People.findOne({email: 'sophia@example.com'});
+  Memberships.insert({
+    personId: person._id,
+    teamId: team._id
+  });
+  
+  // Add Ludwig
+  var person = People.findOne({email: 'ludwig@example.com'});
+  Memberships.insert({
+    personId: person._id,
+    teamId: team._id
+  });
+  
+  // Add Vinny
+  var person = People.findOne({email: 'vinny@example.com'});
+  Memberships.insert({
+    personId: person._id,
+    teamId: team._id
+  });
+  
+  // Add Stacey
+  var person = People.findOne({email: 'stacey@example.com'});
+  Memberships.insert({
+    personId: person._id,
+    teamId: team._id
+  });
+  
+  // Team 2 members
+  var team = Teams.findOne({name: 'Team 2'});
+  // Add Angela
+  var person = People.findOne({email: 'angela@example.com'});
+  Memberships.insert({
+    personId: person._id,
+    teamId: team._id
+  });
+  
+  // Add Casey
+  var person = People.findOne({email: 'casey@example.com'});
+  Memberships.insert({
+    personId: person._id,
+    teamId: team._id
+  });
+  
+  // Add William
+  var person = People.findOne({email: 'william@example.com'});
+  Memberships.insert({
+    personId: person._id,
+    teamId: team._id
+  });
+  
+  // Add Ludwig
+  var person = People.findOne({email: 'ludwig@example.com'});
+  Memberships.insert({
+    personId: person._id,
+    teamId: team._id
+  });  
 }
