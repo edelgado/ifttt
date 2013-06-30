9c9e4c12c45d5d8189b38f25cd551f497239a762
Enrique Delgado <delgado.enrique@gmail.com>
2013-06-29 23:27:46 -0500

    Moved the logic of pairing peolpe to the server.

--- collections/pairings.js
+++ collections/pairings.js
@@ -2,8 +2,3 @@
 // leverage Meteor's collection reactivity and that's just 
 // plain awesome for our main UI.
 Pairings = new Meteor.Collection('pairings');
-Meteor.methods({
-  pairFolks: function(id) {
-    console.log('Hard core pairing action!');
-  }
-});
