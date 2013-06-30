a79245c460e8449695bbe3c98fadad859acacdd4
Enrique Delgado <delgado.enrique@gmail.com>
2013-06-29 21:54:19 -0500

    This will come in handy later on.

---
+++ server/email.js
@@ -0,0 +1,17 @@
+// A method that the client can call, but only runs server-side.
+Meteor.methods({
+  sendEmail: function (to, from, subject, text) {
+    check([to, from, subject, text], [String]);
+
+    // Let other method calls from the same client start running,
+    // without waiting for the email sending to complete.
+    this.unblock();
+
+    Email.send({
+      to: to,
+      from: from,
+      subject: subject,
+      text: text
+    });
+  }
+});
